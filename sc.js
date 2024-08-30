56b00cd270fc97f4274407aff1e4de5fc96a3836
Andrey <domaratskiy1990@gmail.com>
2024-08-30 22:35:12 +0300

    add

--- sc.js
+++ sc.js
@@ -1,6 +1,25 @@
-const menu = document.querySelector(".menu");
-const burger = document.querySelector(".burger");
+document.addEventListener("DOMContentLoaded", function () {
+  //
+  const menu = document.querySelector(".menu");
+  const burger = document.querySelector(".burger");
 
-burger.addEventListener("click", () => {
-  menu.classList.toggle("active");
+  burger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+
+  const modalWindow = document.querySelector(".modal");
+  const modalBtn = document.querySelector(".addTask");
+  const modalBtnClose = document.querySelector(".modal form span");
+  const modalForm = document.querySelector(".modal form");
+  modalBtn.addEventListener("click", () => {
+    modalWindow.style.display = "block";
+  });
+  modalBtnClose.addEventListener("click", () => {
+    modalWindow.style.display = "none";
+  });
+  modalWindow.addEventListener("click", (event) => {
+    if (!modalForm.contains(event.target)) {
+      modalWindow.style.display = "none";
+    }
+  });
 });
