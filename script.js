9ac00806b8d6b7ba6e6468a972632634f39cdb65
Andrey Domaratskiy <38958899+domaratskiy@users.noreply.github.com>
2024-10-29 20:30:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const menu = document.getElementById("nav");
-
 const burger = document.querySelector(".menuBtn");
-
 const socialBtn = document.querySelector(".socialBtn");
+const closeBtn = document.querySelector(".closeBtn");
+
+const menu = document.getElementById("nav");
 const socialList = document.querySelector(".socialUl");
 
 const menuToggle = () => {
   menu.classList.toggle("open");
 };
+
 const socialToggle = () => {
   socialList.classList.toggle("openSocial");
 };
+
+const closeMenuOnClickOutside = (event) => {
+  if (!menu.contains(event.target) && !burger.contains(event.target)) {
+    menu.classList.remove("open");
+  }
+};
+
 burger.addEventListener("click", menuToggle);
 socialBtn.addEventListener("click", socialToggle);
+closeBtn.addEventListener("click", menuToggle);
+document.addEventListener("click", closeMenuOnClickOutside);
